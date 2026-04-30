1411511f1f9e0a6a400480f3e132afe2dc5f5b16
Kelvin <koolkt10@gmail.com>
2026-04-30 16:08:54 +0100

    Fix collectstatic and static files directory

--- startup.sh
+++ startup.sh
@@ -3,7 +3,7 @@ echo "Running migrations..."
 python manage.py migrate --noinput
 
 echo "Collecting static files..."
-python manage.py collectstatic --noinput
+python manage.py collectstatic --noinput --clear
 
 echo "Starting gunicorn..."
 gunicorn recipe_platform.wsgi:application --bind 0.0.0.0:$PORT --workers 2 --timeout 120
